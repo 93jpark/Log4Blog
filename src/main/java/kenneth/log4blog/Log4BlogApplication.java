c48db933bc1f1cb49378fa4e43bd8ff613b4ed62
Joonwoo Park <93jpark@gmail.com>
2023-02-22 01:33:34 +0900

    project init

---
+++ src/main/java/kenneth/log4blog/Log4BlogApplication.java
@@ -0,0 +1,13 @@
+package kenneth.log4blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log4BlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kenneth/log4blog/Log4BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kenneth.log4blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log4BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
